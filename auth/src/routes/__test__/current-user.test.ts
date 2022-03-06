0f5c6390d8abf020fcd360c32058cb731aa52b3f
Raghav Kattel <arg.ahv7@gmail.com>
2022-03-06 14:47:24 +0545

    fixing auth test error

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -10,7 +10,7 @@ it("responds with details of the current user", async () => {
     .send()
     .expect(200);
 
-  expect(response.body.currentuser.email).toEqual("test@test.com");
+  expect(response.body?.currentuser.email).toEqual("test@test.com");
 });
 
 it("responds with null if not authenticated", async () => {
@@ -19,5 +19,5 @@ it("responds with null if not authenticated", async () => {
     .send()
     .expect(200);
 
-  expect(response.body.currentuser).toEqual(null);
+  expect(response.body?.currentuser).toEqual(null);
 });
